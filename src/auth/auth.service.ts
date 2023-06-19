f7dc2cf7d743923441bcf13d152c74449ad7e6da
ahsanripeseed <135618656+ahsanripeseed@users.noreply.github.com>
2023-06-19 12:16:07 +0500

    Guards added for me end point

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   /**
    * Creates a new user with the provided email and password hash.
    *
@@ -17,26 +23,37 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
     //save the new user in the DB
     try {
+      console.log('user', dto);
       const user = await this.prisma.user.create({
         data: {
           email: dto.email,
           hash: hash,
         },
       });
-      delete user.hash;
-      //return the saved user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
+      console.log('instance of error', error.constructor.name);
+      console.log('error message', error.message);
+      console.log('error', error instanceof PrismaClientKnownRequestError);
       if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'p2002') {
+        if (error.code === 'P2002') {
+          // Adjust the code comparison based on the actual error code
           throw new ForbiddenException(
             'User with the same email already exists',
           );
         }
-        throw error;
       }
+      throw error;
     }
   }
+
+  /**
+   * Asynchronously signs in a user with authentication data.
+   *
+   * @param {AuthDto} dto - An object containing the user's email and password.
+   * @return {Promise<User>} The signed in user sans the password hash.
+   * @throws {ForbiddenException} If the credentials are incorrect.
+   */
   async signin(dto: AuthDto) {
     //find the user by email
     const user = await this.prisma.user.findUnique({
@@ -55,8 +72,32 @@ export class AuthService {
       throw new ForbiddenException('Credentials incorrect');
     }
 
-    //send back the user
-    delete user.hash;
-    return user;
+    return await this.signToken(user.id, user.email);
+  }
+
+  /**
+   * Asynchronously signs a token with a user ID and email to create an access token.
+   *
+   * @param {number} userId - The ID of the user to sign the token for.
+   * @param {string} email - The email address of the user to sign the token for.
+   * @return {Promise<{ access_token: string }>} A Promise that resolves to an object containing the access_token.
+   */
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const accessToken = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token: accessToken,
+    };
   }
 }
